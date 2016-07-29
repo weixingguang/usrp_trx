8a8c1f240f0747e957b8e583042b5a1dcf54d1d8
wxg <1132277123@qq.com>
2016-07-29 16:25:24 +0800

    tx

--- tx_double.c
+++ tx_double.c
@@ -9,15 +9,16 @@
 #define FFT_CP_SIZE 16
 
 double complex long_sync_store[64] = {
-	10.00 -0.00j ,  0.33 + 7.70j ,  2.54 -7.11j ,  -6.20 -5.30j ,  1.35 + 1.78j ,  -3.83 + 5.61j ,  -7.37 -3.53j ,  2.45 + 6.79j , 
-	6.24 -1.66j ,  -3.41 -0.26j ,  0.06 -7.36j ,  8.76 + 3.03j ,  1.57 -3.75j ,  -3.75 + 0.96j ,  -1.44 + 10.28j ,  -7.63 + 0.26j , 
-	4.00 -4.00j ,  -2.36 -6.29j ,  -3.66 + 2.52j ,  8.40 -4.17j ,  5.26 + 5.91j ,  -4.45 -0.90j ,  -3.86 + 5.20j ,  3.61 + 1.40j , 
-	-2.24 -9.66j ,  7.80 + 1.06j ,  -8.15 -1.31j ,  -4.80 + 4.74j ,  -0.18 + 3.44j ,  5.88 -7.37j ,  5.87 + 6.78j ,  -0.79 -6.25j , 
-	-10.00 + 0.00j ,  -0.79 + 6.25j ,  5.87 -6.78j ,  5.88 + 7.37j ,  -0.18 -3.44j ,  -4.80 -4.74j ,  -8.15 + 1.31j ,  7.80 -1.06j , 
-	-2.24 + 9.66j ,  3.61 -1.40j ,  -3.86 -5.20j ,  -4.45 + 0.90j ,  5.26 -5.91j ,  8.40 + 4.17j ,  -3.66 -2.52j ,  -2.36 + 6.29j , 
-	4.00 + 4.00j ,  -7.63 -0.26j ,  -1.44 -10.28j ,  -3.75 -0.96j ,  1.57 + 3.75j ,  8.76 -3.03j ,  0.06 + 7.36j ,  -3.41 + 0.26j , 
-	6.24 + 1.66j ,  2.45 -6.79j ,  -7.37 + 3.53j ,  -3.83 -5.61j ,  1.35 -1.78j ,  -6.20 + 5.30j ,  2.54 + 7.11j ,  0.33 -7.70j 
+ 0.156 + -0.000j ,  0.005 + 0.120j ,  0.040 + -0.111j ,  -0.097 + -0.083j ,  0.021 + 0.028j ,  -0.060 + 0.088j ,  -0.115 + -0.055j ,  0.038 + 0.106j , 
+	 0.098 + -0.026j ,  -0.053 + -0.004j ,  0.001 + -0.115j ,  0.137 + 0.047j ,  0.024 + -0.059j ,  -0.059 + 0.015j ,  -0.022 + 0.161j ,  -0.119 + 0.004j , 
+	 0.062 + -0.062j ,  -0.037 + -0.098j ,  -0.057 + 0.039j ,  0.131 + -0.065j ,  0.082 + 0.092j ,  -0.070 + -0.014j ,  -0.060 + 0.081j ,  0.056 + 0.022j , 
+	 -0.035 + -0.151j ,  0.122 + 0.017j ,  -0.127 + -0.021j ,  -0.075 + 0.074j ,  -0.003 + 0.054j ,  0.092 + -0.115j ,  0.092 + 0.106j ,  -0.012 + -0.098j , 
+	 -0.156 + 0.000j ,  -0.012 + 0.098j ,  0.092 + -0.106j ,  0.092 + 0.115j ,  -0.003 + -0.054j ,  -0.075 + -0.074j ,  -0.127 + 0.021j ,  0.122 + -0.017j , 
+	 -0.035 + 0.151j ,  0.056 + -0.022j ,  -0.060 + -0.081j ,  -0.070 + 0.014j ,  0.082 + -0.092j ,  0.131 + 0.065j ,  -0.057 + -0.039j ,  -0.037 + 0.098j , 
+	 0.062 + 0.062j ,  -0.119 + -0.004j ,  -0.022 + -0.161j ,  -0.059 + -0.015j ,  0.024 + 0.059j ,  0.137 + -0.047j ,  0.001 + 0.115j ,  -0.053 + 0.004j , 
+	 0.098 + 0.026j ,  0.038 + -0.106j ,  -0.115 + 0.055j ,  -0.060 + -0.088j ,  0.021 + -0.028j ,  -0.097 + 0.083j ,  0.040 + 0.111j ,  0.005 + -0.120j , 
 };
+
 int long_sync_corr(complex double *receive_data, int receive_data_length, int corr_length)
 {
 	int index = 0;
@@ -39,6 +40,7 @@ int long_sync_corr(complex double *receive_data, int receive_data_length, int co
 		double complex an_result = an/corr_power;
 
 		arr[index] = creal(an_result);
+		//printf("[long] an result index %d result %0.6f\n",index, arr[index]);
 		index++;
 	}
 
@@ -114,27 +116,26 @@ void short_sync_corr(complex double *receive_data, int receive_data_length, int 
 			corr_result += receive_data[index + corr_index] * conj(receive_data[index + corr_index + 16]);
 			corr_power += receive_data[index + corr_index + 16] * conj(receive_data[index + corr_index + 16]);
 		}
-		double an = cabs(corr_result);
-		double complex an_result = an/corr_power;
+		double complex an_result = cabs(corr_result)/corr_power;
 
-		if(cabs(an_result) > 0.75 )
+		if(cabs(an_result) > 0.80 )
 		{
-			printf("[short]    index: %d  corr_result: ( %0.2f + %0.2fj )  %0.4f \n", index, creal(an_result), cimag(an_result), cabs(an_result));
+			printf("[short]    index: %d  corr_result: ( %0.2f + %0.2fj )  %0.4f \n", index+16, creal(an_result), cimag(an_result), cabs(an_result));
 			continus_corr++;
 
 			if(continus_corr == 3)
 			{
 				continus_corr = 0;
-				printf("Find the short peak index [%d] !........ \n", index);
+				printf("Find the short peak index [%d] !........ \n", index+16);
 
-				int long_index = long_sync_corr(&receive_data[index], 320, 64);
+				int long_index = long_sync_corr(&receive_data[index+16], 384, 64);
 				if ( long_index >= 64 )
 				{
-					printf("[LONG] the start samp %d \n", long_index);
+					printf("[LONG] the start samp %d \n", long_index + index + 16);
 					int tmp_index;
 					for(tmp_index = 0; tmp_index < 4; tmp_index++)
 					{
-						printf("[ %0.6f + %0.6f ]  \n", creal(receive_data[long_index+tmp_index]), cimag(receive_data[long_index+tmp_index]));
+						printf("[ %0.6f + %0.6f ]  \n", creal(receive_data[long_index+index+16+tmp_index]), cimag(receive_data[long_index+index+16+tmp_index]));
 					}
 
 					index += 320;
@@ -170,7 +171,7 @@ int main()
 		-1.4719601443879746-1.4719601443879746j, 0.0, 0.0, 0.0, 1.4719601443879746+1.4719601443879746j, 0.0, 0.0, 0.0,
 		0.0, 0.0, 0.0, 0.0, -1.4719601443879746-1.4719601443879746j, 0.0, 0.0, 0.0,
 		-1.4719601443879746-1.4719601443879746j, 0.0, 0.0, 0.0, 1.4719601443879746+1.4719601443879746j, 0.0, 0.0, 0.0,
-		1.4719601443879746+1.4719601443879746j, 0.0, 0.0, 0.0, 1.4719601443879746+1.4719601443879746j, 0.0, 0.0, 0.0,
+		+1.4719601443879746+1.4719601443879746j, 0.0, 0.0, 0.0, 1.4719601443879746+1.4719601443879746j, 0.0, 0.0, 0.0,
 		1.4719601443879746+1.4719601443879746j, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
 	};
 
@@ -183,7 +184,18 @@ int main()
 	 	0, 1, -1, -1, 1, 1, -1, 1, -1, 1, -1, -1, -1, -1, -1, 1, 1, -1, -1, 1, -1, 1, -1, 1, 1, 1, 1, 0, 0, 0, 0, 0
 	};
 
-	complex double phy_sync_ofdm[320] = {0.0};
+	complex double tx_data[64] = {
+		0.5-0.5j,0.5+0.5j,0.5+0.5j,0.5-0.5j,0.5+0.5j,0.5+0.5j,0.5+0.5j,0.5-0.5j,
+		0.5+0.5j,0.5+0.5j,0.0+0.5j,0.5-0.5j,0.5+0.5j,0.5+0.0j,0.5+0.5j,0.5-0.5j,
+		0.5+0.5j,0.5+0.5j,0.0+0.5j,0.5-0.5j,0.5+0.5j,0.5+0.0j,0.5+0.5j,0.5-0.5j,
+		0.5-0.5j,0.5-0.5j,0.0+0.5j,0.5-0.5j,0.5+0.5j,0.5+0.0j,0.5+0.5j,0.5-0.5j,
+		0.5+0.5j,0.5-0.5j,0.0+0.5j,0.5-0.5j,0.5+0.5j,0.5+0.5j,0.5+0.5j,0.5-0.5j,
+		0.5+0.5j,0.5-0.5j,0.0+0.5j,0.5-0.5j,0.5+0.5j,0.5+0.5j,0.5+0.5j,0.5-0.5j,
+		0.5-0.5j,0.5+0.5j,0.5+0.5j,0.5-0.5j,0.5+0.5j,0.5+0.5j,0.5+0.5j,0.5-0.5j,
+		0.5-0.5j,0.5+0.5j,0.5+0.5j,0.5-0.5j,0.5+0.5j,0.5+0.5j,0.5+0.5j,0.5-0.5j
+	};
+
+	complex double phy_sync_ofdm[400] = {0.0};
 
 	fftw_plan fftwp = fftw_create_plan(FFT_SIZE, FFTW_BACKWARD, FFTW_ESTIMATE);
 	fftw_one(fftwp, (fftw_complex *)&short_sync[0], (fftw_complex *)&phy_sync_ofdm[FFT_CP_SIZE]);
@@ -195,7 +207,17 @@ int main()
 
 	fftw_one(fftwp, (fftw_complex *)&long_sync_2[0], (fftw_complex *)&phy_sync_ofdm[256]);
 	memcpy(&phy_sync_ofdm[240], &phy_sync_ofdm[304],  FFT_CP_SIZE * sizeof(complex double));
+
+	fftw_one(fftwp, (fftw_complex *)&tx_data[0], (fftw_complex *)&phy_sync_ofdm[336]);
+	memcpy(&phy_sync_ofdm[320], &phy_sync_ofdm[384],  FFT_CP_SIZE * sizeof(complex double));
+
 	fftw_destroy_plan(fftwp);
+	
+	int i;
+	for(i = 0; i < 400; i++)
+	{
+		phy_sync_ofdm[i] /= 64.0;
+	}
 
 #ifdef _TIME_
 	gettimeofday(&te, NULL);
@@ -208,7 +230,7 @@ int main()
 	int index;
 	for(index = 0; index < 80; index++)
 	{
-		printf("( %0.2f + %0.2fj ) ", creal(phy_sync_ofdm[index]), cimag(phy_sync_ofdm[index]));
+		printf("( %0.3f + %0.3fj ) ", creal(phy_sync_ofdm[index]), cimag(phy_sync_ofdm[index]));
 		if((index + 1)%8 == 0)
 			printf("\n");
 	}
@@ -216,7 +238,7 @@ int main()
 	printf("the ifft result of short sync : \n");
 	for(index; index < 160; index++)
 	{
-		printf("( %0.2f + %0.2fj ) ", creal(phy_sync_ofdm[index]), cimag(phy_sync_ofdm[index]));
+		printf("( %0.3f + %0.3fj ) ", creal(phy_sync_ofdm[index]), cimag(phy_sync_ofdm[index]));
 		if((index + 1)%8 == 0)
 			printf("\n");
 	}
@@ -225,7 +247,16 @@ int main()
 	printf("the ifft result of long sync : \n");
 	for(index; index < 320; index++)
 	{
-		printf("( %0.2f + %0.2fj ) ", creal(phy_sync_ofdm[index]), cimag(phy_sync_ofdm[index]));
+		printf("( %0.3f + %0.3fj ) ", creal(phy_sync_ofdm[index]), cimag(phy_sync_ofdm[index]));
+		if((index + 1)%8 == 0)
+			printf("\n");
+	}
+	printf("\n");
+
+	printf("the ifft result of data : \n");
+	for(index; index < 400; index++)
+	{
+		printf("( %0.3f + %0.3fj ) ", creal(phy_sync_ofdm[index]), cimag(phy_sync_ofdm[index]));
 		if((index + 1)%8 == 0)
 			printf("\n");
 	}
@@ -233,12 +264,7 @@ int main()
 #endif
 
 	complex double phy_sync_test[65536] = {0 + 0j};
-	memcpy(&phy_sync_test[100], &phy_sync_ofdm[0], 320 * sizeof(complex double));
-	phy_sync_test[420] = 1 + 1j;
-	phy_sync_test[421] = -1 + 1j;
-	phy_sync_test[422] = -1 - 1j;
-	phy_sync_test[423] = 1 - 1j;
-
+	memcpy(&phy_sync_test[100], &phy_sync_ofdm[0], 400 * sizeof(complex double));
 	short_sync_corr(phy_sync_test, 2016, 48);
 	//long_sync_corr(phy_sync_test, 2016, 64);
 
